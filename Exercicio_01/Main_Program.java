7ff4f0cc2f60cd1d4d569ed824b369a097f2c291
Carlos Henrique Cunha de Oliveira <carloscunha080@gmail.com>
2023-05-11 21:35:15 -0300

    Folder: Exercicio_01 feat: Adicionado a descrição de qual problema o programa terá que ser  capaz de resolver.

--- Exercicio_01/Main_Program.java
+++ Exercicio_01/Main_Program.java
@@ -1,5 +1,10 @@
 package Exercicio_01;
 
+//Problem 1.
+//Uma escola precisa fazer um levantamento do quantitativo de alunos com deficiência (PCD) para tomar algumas decisões estratégicas. 
+//Você foi contratado para projetar um algoritmo que obtenha o número total de alunos dessa escola e também quantos desses possuem necessidades especiais.
+// Ao final, calcule e apresente o percentual de PCD’s dentre o total de alunos.///
+
 public class Main_Program{
       public static void Main(){
             
