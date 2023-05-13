e31a3d42740ca88faa1ed4379de9d975ba92ca53
Carlos Henrique Cunha de Oliveira <carloscunha080@gmail.com>
2023-05-12 21:19:46 -0300

    feat: Criado nova classe para mostrar informações no terminal. Descrição: Essa classe possui uma série de métodos para mostrar informações no terminal do usuário.

---
+++ Exercicio_01/modules/Messages_Of_Terminal.java
@@ -0,0 +1,34 @@
+package modules;
+
+public class Messages_Of_Terminal {
+    public void Show_Welcome_Message(){
+        String Messages_of_Welcome = "Bem vindo ao SchoolHigh Solutions!";
+        System.out.println(Messages_of_Welcome);
+    }
+    public void Show_Message_To_User_Input_Quantity_Students_With_PWD(){
+        String Message_To_User_Input_Quantity_Students_With_PWD="Por favor, digite a quantidade de alunos que possuem deficiência: ";
+        System.out.print(Message_To_User_Input_Quantity_Students_With_PWD);
+    }
+    public void Show_Message_To_User_Input_Quantity_Students_With_Not_PWD(){
+        String Message_To_User_Input_Quantity_Students_With_Not_PWD="Por favor, digite a quantidade de alunos que n?o possuem deficiência: ";
+        System.out.print(Message_To_User_Input_Quantity_Students_With_Not_PWD);
+    }
+    public void Show_Total_of_Students_In_The_School(double Total_of_Students){
+        String Message_Total_of_Students = String.format("O total de alunos que estudam na escola é: %d", Total_of_Students);
+        System.out.println(Message_Total_of_Students);
+    }
+    public void Show_Total_of_Students_In_The_School_With_PWD_And_Percentage(double Students_With_PWD, Double Percentage){
+        String Message_Total_of_Students = String.format(
+            "O total de alunos que POSSUEM deficiência é de %.0f. Isso representa uma porcentagem de %.2f porcento do total de alunos.", 
+                    Students_With_PWD,Percentage
+            );
+        System.out.println(Message_Total_of_Students);
+    }
+    public void Show_Total_of_Students_In_The_School_With_Not_PWD_and_Percentagem(double Students_With_Not_PWD, Double Percentage){
+        String Message_Total_of_Students = String.format(
+            "O total de alunos que N?O POSSUEM deficiência é de %.0f. Isso representa uma porcentagem de %.2f porcento do total de alunos.", 
+            Students_With_Not_PWD,Percentage
+            );
+        System.out.println(Message_Total_of_Students);
+    }
+}
