af802247bf29725f0a63f03cbadfb06c7a34330a
Carlos Henrique Cunha de Oliveira <carloscunha080@gmail.com>
2023-05-14 12:07:12 -0300

    feat: Criado construtor para alimentar a classe mãe polygon.
    Descriçao: Adicionado também campo base e height.

--- Exercicio_03/Geometry/Rectangle.java
+++ Exercicio_03/Geometry/Rectangle.java
@@ -1,4 +1,13 @@
 package Geometry;
 
-public class Rectangle {
+public class Rectangle extends Polygon{
+
+    double Base;
+    double Height;
+    public Rectangle(double Base, double Height ){
+
+        super(4,4,4,2);
+        this.Base=Base;
+        this.Height= Height;
 }
+
