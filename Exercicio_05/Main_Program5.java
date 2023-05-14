04869977ca00c22f53208a9581d2954dec8e3301
Carlos Henrique Cunha de Oliveira <carloscunha080@gmail.com>
2023-05-14 15:24:56 -0300

    feat: Criado diretório para criação do algoritmo para o exercicio 5.

---
+++ Exercicio_05/Main_Program5.java
@@ -0,0 +1,13 @@
+
+//Um consultório médico lhe solicitou um aplicativo para emissão de receituário de pacientes.
+//O funcionamento é o seguinte: o médico informa o nome do paciente e, a seguir, digita o nome e a dosagem dos remédios (remédio 1, 3x ao dia, remédio 2, 12 em 12 hs, etc.). 
+//A leitura se encerra quando o médico digitar a palavra ?FIM? no nome do remédio. 
+//Ao final, uma mensagem de sucesso deve ser apresentada ao médico e o programa terminar após a impressão do receituário digitado na tela. 
+//Apresente um algoritmo que resolva o problema apresentado. 
+public class Main_Program5 {
+      
+      public static void main(String[] args) throws InterruptedException {
+
+      }
+}
+
