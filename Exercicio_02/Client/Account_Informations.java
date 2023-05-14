ae0891f141cb25a8376cf51c61a93ae7ce6d590e
Carlos Henrique Cunha de Oliveira <carloscunha080@gmail.com>
2023-05-14 10:44:57 -0300

    feat: Criado novo construtor.
    Descrição: Criei um novo construtor para pode atribuir uma entidade ao campo Client e também para inicializar o campos Historic_of_Transactions.

--- Exercicio_02/Client/Account_Informations.java
+++ Exercicio_02/Client/Account_Informations.java
@@ -1,9 +1,10 @@
 package Client;
 
-import modules.payments_context.Payments;
-import modules.payments_context.Payments_Information_Model;
+
+import payments_solutions.Payments_Information_Model;
 
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class Account_Informations {
@@ -13,6 +14,11 @@ public class Account_Informations {
     private String Password;
     private Collection<Payments_Information_Model> Historic_of_Transactions;
 
+    public Account_Informations(Client_Model Client){
+
+        this.Client = Client;
+        this.Historic_of_Transactions = new ArrayList<Payments_Information_Model>();
+    }
     public boolean Add_Balance (Payments_Information_Model Payment_Information){
         try{
 
@@ -26,17 +32,14 @@ public class Account_Informations {
     public boolean Remove_Balance (Payments_Information_Model Payment_Information){
         double Quantity_to_be_Removed_Of_Account = Payment_Information.GetterQuantity_Of_Money_Transaction();
 
-        boolean Can_Be_Removed_Of_Account = this.Verify_If_Its_Possible_Remove_Balance_Of_Account(Quantity_to_be_Removed_Of_Account);
-        if(Can_Be_Removed_Of_Account){
-            this.Historic_of_Transactions.add(Payment_Information);
-            this.Value_Total_In_Account -=Payment_Information.GetterQuantity_Of_Money_Transaction();
-            return true;
-        }else{
-            return false;
-        }
+        this.Historic_of_Transactions.add(Payment_Information);
+        this.Value_Total_In_Account -=Payment_Information.GetterQuantity_Of_Money_Transaction();
+        return true;
+
 
     }
     private boolean Verify_If_Its_Possible_Remove_Balance_Of_Account (Double Quantity_To_Be_Removed){
+
         if(this.Value_Total_In_Account>0){
             Double Total_Available_To_Remove_Of_Account = this.Value_Total_In_Account + this.Overdraft;
             if(Total_Available_To_Remove_Of_Account>=Quantity_To_Be_Removed){
