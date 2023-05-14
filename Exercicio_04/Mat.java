3c0e4f882c54a195bbac28b3a227b4ae115fdf77
Carlos Henrique Cunha de Oliveira <carloscunha080@gmail.com>
2023-05-14 15:13:44 -0300

    feat: Criei uma classe para representar uma esteira.
    Descrição: Essa classe representa a esteira. Sempre que é adicionado um item a ela, é verificado se um relógio ou não. Se for, é adicionado List_Of_Watchs_Passed_In_Mat, se não, é retornal false.

---
+++ Exercicio_04/Mat.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class Mat {
+    ArrayList<Items> List_Of_Watchs_Passed_In_Mat = new ArrayList<Items>();
+
+    public void Add_Item_Will_Pass_In_MaT(Items item_will_pass) {
+
+        this.List_Of_Watchs_Passed_In_Mat.add(item_will_pass);
+
+    }
+
+    public boolean Verify_If_Its_Watch(Items item) {
+        if (item.equals(Items.WATCH)) {
+            this.Add_Item_Will_Pass_In_MaT(item);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
