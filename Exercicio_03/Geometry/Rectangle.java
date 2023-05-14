689b56588b7964442e8b6f78004b0d4e24b813c7
Carlos Henrique Cunha de Oliveira <carloscunha080@gmail.com>
2023-05-14 12:10:45 -0300

    bug: Adicionei a } que estava faltando para a execução do programa.

--- Exercicio_03/Geometry/Rectangle.java
+++ Exercicio_03/Geometry/Rectangle.java
@@ -1,19 +1,22 @@
 package Geometry;
 
-public class Rectangle extends Polygon{
+public class Rectangle extends Polygon {
 
     double Base;
     double Height;
-    public Rectangle(double Base, double Height ){
 
-        super(4,4,4,2);
-        this.Base=Base;
-        this.Height= Height;
+    public Rectangle(double Base, double Height) {
+
+        super(4, 4, 4, 2);
+        this.Base = Base;
+        this.Height = Height;
     }
 
-    public double Calculate_Perimeter(){
-        return 2*(this.Base+this.Height);
+    public double Calculate_Perimeter() {
+        return 2 * (this.Base + this.Height);
     }
-    public double Calculate_Area(){
-        return this.Height*Base;
+
+    public double Calculate_Area() {
+        return this.Height * Base;
     }
+}
