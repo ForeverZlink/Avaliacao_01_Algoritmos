633f2a53708bd6f0e818a4372eb3d92385efe742
Carlos Henrique Cunha de Oliveira <carloscunha080@gmail.com>
2023-05-14 12:13:44 -0300

    feat: Criado diretório para resolução do exercicio 4

---
+++ Exercicio_04/Main_Program4.java
@@ -0,0 +1,18 @@
+
+
+
+//Você foi contratado para desenvolver um algoritmo para ajudar uma indústria fabricante de relógios a contabilizar sua produção.
+// A solução precisa contar cada peça produzida e, ao final, informar ao usuário o número total.
+// Quem faz a entrada de dados é um sensor que identifica cada relógio que passa na esteira.
+// O algoritmo só se encerra quando um item que não seja relógio for identificado. É neste momento que o total deve ser apresentado na tela.
+public class Main_Program4 {
+      
+      public static void main(String[] args){
+
+
+
+
+
+            
+      }
+}
