0d6ea9e97459762f166d579792c271f69b25b964
Carlos Henrique Cunha de Oliveira <carloscunha080@gmail.com>
2023-05-14 12:09:26 -0300

    feat: Criado instância do tipo Rectangle.
    Descrição: Após isso, formatei um message com os valores do perimetro e da area do retangulo para mostrar a mensagem do usuário.

--- Exercicio_03/Main_Program3.java
+++ Exercicio_03/Main_Program3.java
@@ -1,20 +1,32 @@
+import Geometry.Rectangle;
 import utils.Inputs_Handler;
 
 
 //3.
-//Um professor de matemática precisa de um software para ajudar no ensino de geometria.
-// Sua ideia é ter uma ferramenta em que ele vai colocar as dimensões de um determinado polígono e o software apresentará as respectivas área e perímetro.
-// Para testar sua ideia, ele optou por começar com retângulos e lhe pediu ajuda no desenvolvimento. 
-//Assim sendo, faça um algoritmo que receba as dimensões de base e altura de um retângulo e, ao final, apresente os valores de seu perímetro e área.
+//Um professor de matem?tica precisa de um software para ajudar no ensino de geometria.
+// Sua ideia ? ter uma ferramenta em que ele vai colocar as dimens?es de um determinado pol?gono e o software apresentar? as respectivas ?rea e per?metro.
+// Para testar sua ideia, ele optou por come?ar com ret?ngulos e lhe pediu ajuda no desenvolvimento. 
+//Assim sendo, fa?a um algoritmo que receba as dimens?es de base e altura de um ret?ngulo e, ao final, apresente os valores de seu per?metro e ?rea.
 public class Main_Program3{
       
       public static void main(String[] args){
+
+
          Inputs_Handler input = new Inputs_Handler();
          System.out.print("Digite a base do retangulo: ");
          double base = input.Read_Input_Of_Double();
 
          System.out.print("Digite a altura do retangulo: ");
          double height = input.Read_Input_Of_Double();
+
+         Rectangle Rectangle_Instance = new Rectangle(base,height);
+         String Message_Formated =String.format(
+                 "O perimetro desse retangulo é %.2f e a sua área %.2f",
+                 Rectangle_Instance.Calculate_Perimeter(),Rectangle_Instance.Calculate_Area()
+         );
+         System.out.println(Message_Formated);
+
+
             
       }
 }
